9124942b7b5ab8edbaf3c5dee9f2e9447f573bb7
Duckie08 <52843939+Duckie08@users.noreply.github.com>
2021-10-08 21:00:29 +0700

    chore: handling validation error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,6 +4,17 @@ const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
+
+const handleDuplicateDB = err => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value name ${value}`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -18,8 +29,9 @@ const sendErrorProd = (err, res) => {
       status: err.status,
       message: err.message
     });
+
+    //////
   } else {
-    console.error('error', err);
     res.status(500).json({
       status: 'error',
       message: 'Something went wrong'
@@ -37,6 +49,9 @@ module.exports = (err, req, res, next) => {
     let error = Object.assign(err);
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error, res);
 
     sendErrorProd(error, res);
   }
