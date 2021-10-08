2a209c9b34cf080d0484f6fa75d4eed1cfc4c9a3
Duckie08 <52843939+Duckie08@users.noreply.github.com>
2021-10-08 21:28:31 +0700

    chore: catching uncaught exception

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT');
+  console.log(err.name, err.message);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -18,8 +23,13 @@ mongoose
 
 // console.log(process.env);
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App runniung on prot ${port}`);
 });
 
-//TEST
+process.on('unhandledRejection', err => {
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
