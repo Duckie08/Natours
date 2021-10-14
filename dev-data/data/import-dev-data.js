2187dbaf99b69450bb392982b4f7827ee9297676
Duckie08 <52843939+Duckie08@users.noreply.github.com>
2021-10-15 06:09:03 +0700

    fix: fixing import and delete data error

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -20,11 +22,17 @@ mongoose
 
 //read json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf8')
+);
 
 //import data to db
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('data successfully loaded');
   } catch (err) {
     console.log(err);
@@ -32,11 +40,26 @@ const importData = async () => {
   process.exit();
 };
 
-//DELETE ALL DATA FROM DB
+// DELETE ALL DATA FROM DB
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+
     console.log('Delete successfully');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const resetData = async () => {
+  try {
+    await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log('Data successfuly DELETED!');
   } catch (err) {
     console.log(err);
   }
@@ -45,8 +68,13 @@ const deleteData = async () => {
 
 if (process.argv[2] === '--import') {
   importData();
-} else if (process.argv[2] === 'delete') {
+} else if (process.argv[2] === '--delete') {
   deleteData();
+} else if (process.argv[2] === '--reset') {
+  resetData();
 }
 
-// console.log(process.argv);
+//To deletedata use
+//$ npm run resetD
+
+//password for all users: test 1234
