bf47f2a7bf4e8248aa2528ade90fee278b07c141
Duckie08 <52843939+Duckie08@users.noreply.github.com>
2021-10-16 23:52:08 +0700

    fix: preventing duplicate review and rounded avgRating

--- models/tourModel.js
+++ models/tourModel.js
@@ -37,7 +37,8 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       default: 4.5,
       min: [1, 'Rating must be abbove 1.'],
-      max: [5, 'Rating must be below 5']
+      max: [5, 'Rating must be below 5'],
+      set: val => Math.round(val * 10) / 10
     },
     ratingQuantity: {
       type: Number,
