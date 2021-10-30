4c8b3abf71c79bac69f957d1fd3614737bc072bb
Duckie08 <52843939+Duckie08@users.noreply.github.com>
2021-10-31 04:14:52 +0700

    test

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+// /*eslint-disable*/
+// import { login } from './login';
+// import '@babel/polyfill';
+// import { displayMap } from './mapbox';
+
+// const mapBox = document.getElementById('map');
+// const loginForm = document.getElementById('map');
+
+// const email = document.getElementById('email').value;
+// const password = document.getElementById('password').value;
+
+// if (mapBox) {
+//   const locations = JSON.parse(mapBox.dataset.locations);
+//   displayMap(locations);
+// }
+
+// if (loginForm) {
+//   loginForm.addEventListener('submit', e => {
+//     e.preventDefault();
+
+//     login(email, password);
+//   });
+// }
